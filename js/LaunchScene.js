3d371c586cdc3c14bd1da5d79e1fec9e41f9ecda
HallowedDust5 <charizardexlvl100@gmail.com>
2022-02-11 22:50:35 -0500

    Added placeholders for stats assets

--- js/LaunchScene.js
+++ js/LaunchScene.js
@@ -1,5 +1,9 @@
 class LaunchScene extends Phaser.Scene{
 
+    /**
+     * 
+     * @param {Object} stats Pass the global stats objects through 
+     */
     constructor(stats){
         super({key:'launch scene'});
         this.stats = stats;
@@ -11,8 +15,11 @@ class LaunchScene extends Phaser.Scene{
         /*
         Load needed assets here
         */ 
+       
 
         this.objects = {};
+
+
     }
 
     create(){
@@ -20,16 +27,42 @@ class LaunchScene extends Phaser.Scene{
         const GAME_HEIGHT = this.sys.game.config.height;
         const GAME_WIDTH = this.sys.game.config.width; 
 
-        function nextTurn() {
+        const MAX_RESOURCE = 10;
+
+
+
+        //Adds background for stats buttons
+        objs.backRect = this.add.rectangle(GAME_WIDTH/2,GAME_HEIGHT/2,GAME_WIDTH/1.4,GAME_HEIGHT/6,0xffffff);
+
+        //First stat's rectangle x coord
+        let first_stat_x = objs.backRect.x - objs.backRect.x/1.8;
+        objs.tool_rects = [];
+        
+        //Generates rectangles for all stats
+        //THIS IS MEANT TO BE CHANGED LATER
+        //whether that be the colors, this is only a basic layout
+        for (let i = 1; i <= Object.keys(this.stats).length; i++) {
+            objs.tool_rects.push(this.add.rectangle(first_stat_x,objs.backRect.y,objs.backRect.width/5.3,objs.backRect.height/1.3,0x000000));
             
-            //Load next scene first
-            this.destroy();
+            first_stat_x+= objs.backRect.width/5.2;
         }
+        
+        let tools_button_onClick = () =>{};
+        let food_button_onClick = () =>{};
+        let medicine_button_onClick = () =>{};
+        let oxygen_button_onClick = () =>{};
+        let water_button_onClick = () =>{};
 
-
-        objs.nextTurnButton = new Button(GAME_WIDTH*(7/8),GAME_HEIGHT*(7/8)+20,'Next Turn',this,nextTurn);
 
         
+        objs.tools_button = new Button()
+        objs.food_button = 
+        objs.medicine_button = 
+        objs.oxygen_button = 
+        objs.water_button = 
+ 
+
+
         
     }
 
--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 
-//These items will be in the inventory set in the stats objects
-var items = [
-    'water purifier',
-    ''
-];
+
 
 var stats = {
     tools:0,
@@ -11,7 +7,6 @@ var stats = {
     medicine:0,
     oxygen:0,
     water:0,
-    inventory: new Set(),
 };
 
 const config = {
