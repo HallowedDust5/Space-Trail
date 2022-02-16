4b44999a37c7f31ff29f23d12f16ad645c6a6254
HallowedDust5 <charizardexlvl100@gmail.com>
2022-02-16 16:08:02 -0500

    Added BaseEvent class

---
+++ js/BaseEvent.js
@@ -0,0 +1,15 @@
+class BaseEvent extends BaseScene{
+
+
+    constructor(stats,key){
+        super(stats,key);
+
+        /**
+         * {
+         * 
+         * }
+         */
+        this.assetPaths
+    }
+
+}
