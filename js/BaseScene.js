4deb6957af5e883cb7652c99dba22d360a139bf2
HallowedDust5 <charizardexlvl100@gmail.com>
2022-02-19 11:54:30 -0500

    Implemented Low Oxygen Event

--- js/BaseScene.js
+++ js/BaseScene.js
@@ -8,7 +8,7 @@ class BaseScene extends Phaser.Scene{
     constructor(stats,key){
         super({key:key});
         this.key = key;
-        this.stats = stats.resources;
+        this.stats = stats;
         this.objects = {};
     }
 
